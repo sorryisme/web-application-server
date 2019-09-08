3919e7c46fc8f350f9ae232e5a9684b98845d1af
sorryisme <wivipp39@gmail.com>
2019-09-08 15:02:11 +0900

    190908 url & parameter GET

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,21 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,12 +30,40 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        try (
+                InputStream in = connection.getInputStream(); 
+                OutputStream out = connection.getOutputStream();
+                ) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+            String reqContents = br.readLine();
+            log.info("req line :{}" , reqContents);
+            if(reqContents == null) {
+                return;
+            }
+            
+            String[] tokens =  reqContents.split(" ");
+            
+            while(!reqContents.equals("")) {
+                reqContents = br.readLine();
+                log.debug("header : {}" , reqContents);
+            }
+            
+            String url = tokens[1];
+            
+            if(url.startsWith("/user/create")) {
+                int index = url.indexOf("?");
+                String queryString = url.substring(index+1);
+                Map<String, String> params = 
+                        HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"),params.get("email"));
+                log.debug("User : {}", user);
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+    //            byte[] body = "Hello World".getBytes();
+                byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
