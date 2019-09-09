c2d5e023cc0bed222e83fb90ee0d6cea9e150316
sorryisme <wivipp39@gmail.com>
2019-09-09 21:46:50 +0900

    190909 302 stauts redirection

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -63,6 +63,10 @@ public class RequestHandler extends Thread {
                         HttpRequestUtils.parseQueryString(body);
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"),params.get("email"));
                 log.debug("User : {}", user);
+                
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos, "/index.html");
+                
             } else {
                 DataOutputStream dos = new DataOutputStream(out);
     //            byte[] body = "Hello World".getBytes();
@@ -98,5 +102,15 @@ public class RequestHandler extends Thread {
     private int getContentLength(String line) {
         String[] headerTokens = line.split(":");
         return Integer.parseInt(headerTokens[1].trim());
+    }
+    
+    private void response302Header(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + url + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 }
